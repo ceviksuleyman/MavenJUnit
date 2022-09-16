9945b1f7de2d13e224daf9dc90e427cc95fea257
cevik <suleymancevik64@gmail.com>
2022-09-17 02:11:31 +0300

    22-09-17

--- src/test/java/automation_exercise/TC03.java
+++ src/test/java/automation_exercise/TC03.java
@@ -1,5 +1,17 @@
 package automation_exercise;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
 public class TC03 {
     /*
      1. Tarayıcıyı başlatın
@@ -11,4 +23,54 @@ public class TC03 {
      7. 'Giriş' düğmesini tıklayın
      8. 'E-postanız veya şifreniz yanlış!' hatasını doğrulayın. görünür
      */
+    WebDriver driver;
+
+    @Test
+    public void test01() throws InterruptedException {
+        //Ana sayfanın başarıyla görünür olduğunu doğrulayın
+        Assert.assertTrue(driver.findElement(By.xpath("//*[@class='fa fa-home']")).isDisplayed());
+
+
+        //'Kayıt Ol / Giriş Yap' düğmesine tıklayın
+        driver.findElement(By.xpath("//*[@class='fa fa-lock']")).click();
+        Thread.sleep(1000);
+
+
+        // 'Hesabınıza giriş yapın' ifadesinin görünür olduğunu doğrulayın
+        Assert.assertTrue(driver.findElement(By.xpath("//*[.='Login to your account']")).isDisplayed());
+        String loginToYA = driver.findElement(By.xpath("//*[.='Login to your account']")).getText();
+        System.out.println("Gorunen Ifade -> " + loginToYA);
+
+
+        // Yanlış e-posta adresi ve şifre girin
+        driver.findElement(By.xpath("//*[@data-qa='login-email']")).sendKeys("aliveli@gmail.com");
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("//*[@data-qa='login-password']")).sendKeys("12343");
+        Thread.sleep(1000);
+
+
+        //'Giriş' düğmesini tıklayın
+        driver.findElement(By.xpath("//*[@data-qa='login-button']")).click();
+
+
+        //'E-postanız veya şifreniz yanlış!' hatasını doğrulayın. görünür
+        WebElement girisHatasi = driver.findElement(By.xpath("//*[.='Your email or password is incorrect!']"));
+        System.out.println("hata mesaji -> " + girisHatasi.getText());
+        Assert.assertTrue(girisHatasi.isDisplayed());
+    }
+
+    @Before
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(11));
+        driver.get("http://automationexercise.com");
+    }
+
+    @After
+    public void tearDown() throws InterruptedException {
+        Thread.sleep(2000);
+        driver.close();
+    }
 }
---
+++ src/test/java/automation_exercise/TC04.java
@@ -0,0 +1,16 @@
+package automation_exercise;
+
+public class TC04 {
+    /*
+     1. Tarayıcıyı başlatın
+     2. 'http://automationexercise.com' url'sine gidin
+     3. Ana sayfanın başarıyla görünür olduğunu doğrulayın
+     4. 'Kayıt Ol / Giriş Yap' düğmesine tıklayın
+     5. 'Hesabınıza giriş yapın' ifadesinin görünür olduğunu doğrulayın
+     6. Doğru e-posta adresini ve şifreyi girin
+     7. 'Giriş' düğmesini tıklayın
+     8. 'Kullanıcı adı olarak oturum açıldı' ifadesinin görünür olduğunu doğrulayın
+     9. 'Çıkış' düğmesini tıklayın
+     10. Kullanıcının giriş sayfasına yönlendirildiğini doğrulayın
+     */
+}
