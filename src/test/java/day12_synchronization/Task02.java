4fbebd6b4485def2506ae26f43304122de1ed07e
cevik <suleymancevik64@gmail.com>
2022-09-23 01:21:52 +0300

    22-09-23

--- src/test/java/day12_synchronization/Task02.java
+++ src/test/java/day12_synchronization/Task02.java
@@ -1,6 +1,9 @@
 package day12_synchronization;
 
+import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import utilities.TestBaseBeforeAfter;
 
 public class Task02 extends TestBaseBeforeAfter {
@@ -14,8 +17,45 @@ public class Task02 extends TestBaseBeforeAfter {
     "http://webdriveruniversity.com/index.html" adresine gittigini test edin
     */
     @Test
-    public void test01() {
+    public void test01() throws InterruptedException {
+
+        //“http://webdriveruniversity.com/IFrame/index.html” sayfasina gidin
+        driver.get("http://webdriveruniversity.com/IFrame/index.html");
+
+        //“Our Products” butonuna basin
+        WebElement iframe = driver.findElement(By.cssSelector("#frame"));
+        driver.switchTo().frame(iframe);
+        Thread.sleep(2000);
+
+        driver.findElement(By.xpath("//*[text()='Our Products']")).click();
 
 
+        //“Cameras product” i tiklayin
+        driver.findElement(By.cssSelector("#camera-img")).click();
+        Thread.sleep(2000);
+
+
+        //Popup mesajini yazdirin
+        System.out.println("Mesaj -> " + driver.findElement(By.xpath("//*[@class='modal-body']")).getText());
+        Thread.sleep(2000);
+
+
+        //“close” butonuna basin
+        driver.findElement(By.xpath("//*[text()='Close']")).click();
+        Thread.sleep(2000);
+
+        driver.switchTo().defaultContent();
+
+        //"WebdriverUniversity.com (IFrame)" linkini tiklayin
+        driver.findElement(By.xpath("(//a[@class='navbar-brand'])[1]")).click();
+        Thread.sleep(2000);
+
+
+        //"http://webdriveruniversity.com/index.html" adresine gittigini test edin
+        String expectedUrl = "http://webdriveruniversity.com/index.html";
+        Assert.assertEquals(expectedUrl, driver.getCurrentUrl());
+
+
+        driver.quit();
     }
 }
